00edcb1c726ccd735e0bc120945d1d377c3e6264
Ruslan <ruslan-dogadaev@list.ru>
2024-03-15 13:14:08 +0500

    this firist

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class= "nav/logo" title="Back to Homepage">Руслан Догадаев</a>
+        <div class="nav-links">
+            <a href="/about" class="link">Обо мне</a>
+            <a href="/blog" class="link">Блог</a>
+            <a href="/project" class="project">Портфолио</a>
+            <a href="/contact" class="contact">Контакты</a>
+        </div>
+    </div>
+</nav>
+<div class="container">
+   <slot></slot>
+</div>
+<style>
+    .container{
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
